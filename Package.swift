794421114ba17fe12f26703180ef4bf8d313227f
njohnston-daon <njohnston@daon.com>
2023-08-08 13:58:30 -0400

    Update built framework location

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "libtesseract",
-      url: "https://github.com/SwiftyTesseract/libtesseract/releases/download/0.2.0/libtesseract-0.2.0.xcframework.zip",
-      checksum: "cc42f3424047adc7064e6bb67d5039385629ee42199fcbb0553f57f1110d8c90"
+      url: "https://github.com/njohnston-daon/libtesseract/releases/download/0.2.0/libtesseract-ios-4.1.3.xcframework.zip",
+      checksum: "634baae2438d6f355b2b303e2b813d18dc345f2b5ef7bfa3db0fadad4ca44993"
     )
   ],
   swiftLanguageVersions: [.v5]
