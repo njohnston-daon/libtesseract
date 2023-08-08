80383b493fc4acadd6c002ab19194aa4bbf68f1d
njohnston-daon <njohnston@daon.com>
2023-08-08 14:35:27 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "libtesseract",
-      url: "https://github.com/njohnston-daon/libtesseract/releases/download/0.2.0/libtesseract-ios-4.1.3.xcframework.zip",
+      url: "https://github.com/njohnston-daon/libtesseract/releases/download/0.2.0/libtesseract-0.2.0.xcframework.zip",
       checksum: "634baae2438d6f355b2b303e2b813d18dc345f2b5ef7bfa3db0fadad4ca44993"
     )
   ],
